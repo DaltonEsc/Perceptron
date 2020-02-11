b0690bd57a60f3ba88be46fd5f30ae9afb0ac289
Dalton Eschberger <eschberger.dalton@gmail.com>
2020-02-11 13:13:45 -0600

    added finalize class to print data with output and percent

--- src/Finalize.py
+++ src/Finalize.py
@@ -1,9 +1,18 @@
+import numpy as np
+
 class Finalize():
 
     def __init__(self, output):
-        self.final = self.readable(output)
+        self.final = 0
+        self.percent = 0
+        self.readable(output)
 
     def readable(self, output):
-
         
-        return output
+        if output >.5:
+            self.final = np.ceil(output)
+            self.percent = np.dot(output,100)
+        else:
+            self.final = np.floor(output)
+            self.percent = np.dot((1-output), 100)
+        
--- src/NeuralNetwork_tester.py
+++ src/NeuralNetwork_tester.py
@@ -1,7 +1,7 @@
 import numpy as np
-from NeuralNetwork import NeuralNetwork
 
-        
+from NeuralNetwork import NeuralNetwork
+from Finalize import Finalize
 
 if __name__=='__main__':
     training_inputs = np.array([[0,0,1],
@@ -14,13 +14,13 @@ if __name__=='__main__':
         print(training_inputs[i],training_outputs[i])
 
     network = NeuralNetwork()
-    print(network.synaptic_weights)
+    
     network.train(training_inputs, training_outputs, 10000)
-    print(network.synaptic_weights)
-    A = str(input("Input 1:"))
-    B = str(input("Input 1:"))
-    C = str(input("Input 1:"))
 
-    print("New Situation: Input = ", A,B,C)
-    print("Output Data:")
-    print(network.think(np.array([A,B,C])))
+    A = str(input("Input 1:"))
+    B = str(input("Input 2:"))
+    C = str(input("Input 3:"))
+
+    output=Finalize(network.think(np.array([A,B,C])))
+    
+    print("Answer is: ", output.final, " with ", output.percent,"% certainty")
